e312c26ecd9018a1b855315200a1a0f9278ff997
caidenrev <cimicimicilung4868@gmail.com>
2025-08-24 20:20:08 +0700

    gatau lah

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,7 +63,7 @@ class User extends Authenticatable implements FilamentUser
     // ==========================================================
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->role === 'admin';
+        return $this->role === 'admin' || $this->role === 'instructor';
     }
 
     /**
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
-            ->id('admin')
+            ->id('admin', 'instructor')
             ->path('admin')
             ->login()
             ->colors([
